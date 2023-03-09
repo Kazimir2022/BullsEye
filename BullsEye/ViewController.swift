c9d5cf4bb671ff40ffd791e8890697b5f8e178f3
Kazimir Makaranka <m2014ky@gmail.com>
2023-03-09 10:41:10 +0300

    add icons

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -96,6 +96,17 @@ class ViewController: UIViewController {
         score = 0
         round = 0
      startNewRound()
+        
+        let transition = CATransition()
+          transition.type = CATransitionType.fade
+          transition.duration = 1
+          transition.timingFunction = CAMediaTimingFunction(
+            name: CAMediaTimingFunctionName.easeOut)
+          view.layer.add(transition, forKey: nil)
+        
+        
+        
+        
     }
     
     
